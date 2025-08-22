d2d4b08d3c6153659c5ea8cd2c960ecf4ecae2f7
Prajwaml <163293330+Prajwaml@users.noreply.github.com>
2025-08-22 14:19:17 +0530

    Add files via upload

---
+++ build/CMakeFiles/stock-tracker.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/prajwal/stock-tracker/src/alert_manager.cpp" "CMakeFiles/stock-tracker.dir/src/alert_manager.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/alert_manager.cpp.o.d"
+  "/home/prajwal/stock-tracker/src/api.cpp" "CMakeFiles/stock-tracker.dir/src/api.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/api.cpp.o.d"
+  "/home/prajwal/stock-tracker/src/display.cpp" "CMakeFiles/stock-tracker.dir/src/display.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/display.cpp.o.d"
+  "/home/prajwal/stock-tracker/src/logger.cpp" "CMakeFiles/stock-tracker.dir/src/logger.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/logger.cpp.o.d"
+  "/home/prajwal/stock-tracker/src/main.cpp" "CMakeFiles/stock-tracker.dir/src/main.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/main.cpp.o.d"
+  "/home/prajwal/stock-tracker/src/watchlist_manager.cpp" "CMakeFiles/stock-tracker.dir/src/watchlist_manager.cpp.o" "gcc" "CMakeFiles/stock-tracker.dir/src/watchlist_manager.cpp.o.d"
+  "" "stock-tracker" "gcc" "CMakeFiles/stock-tracker.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/stock-tracker.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stock-tracker.dir/link.d"
+  "CMakeFiles/stock-tracker.dir/src/alert_manager.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/alert_manager.cpp.o.d"
+  "CMakeFiles/stock-tracker.dir/src/api.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/api.cpp.o.d"
+  "CMakeFiles/stock-tracker.dir/src/display.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/display.cpp.o.d"
+  "CMakeFiles/stock-tracker.dir/src/logger.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/logger.cpp.o.d"
+  "CMakeFiles/stock-tracker.dir/src/main.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/main.cpp.o.d"
+  "CMakeFiles/stock-tracker.dir/src/watchlist_manager.cpp.o"
+  "CMakeFiles/stock-tracker.dir/src/watchlist_manager.cpp.o.d"
+  "stock-tracker"
+  "stock-tracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stock-tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
